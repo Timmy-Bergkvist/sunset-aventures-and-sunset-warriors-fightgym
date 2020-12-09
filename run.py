27ff92189ab47f1307e761deac70982045abbcec
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-12-09 08:46:40 +0100

    added gitignore file and spell checking

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ import os
 from flask import Flask, render_template, redirect, url_for, request
 
 from sunsetAdventures.routes import sunsetAdventures
-from sunsetWariorrs.routes import sunsetWariorrs
+from sunsetWarriors.routes import sunsetWarriors
 
 app = Flask(__name__)
 app.register_blueprint(sunsetAdventures, url_prefix="/sunsetAdventures")
-app.register_blueprint(sunsetWariorrs, url_prefix="/sunsetWariorrs")
+app.register_blueprint(sunsetWarriors, url_prefix="/sunsetWarriors")
 
 @app.route("/")
 def index():
--- sunsetWarriors/routes.py
+++ sunsetWarriors/routes.py
@@ -1,29 +1,29 @@
 from flask import Blueprint
 from flask import Flask, render_template, redirect, url_for, request
 
-sunsetWariorrs = Blueprint("sunsetWariorrs", __name__, static_folder="static", template_folder="templates")
+sunsetWarriors = Blueprint("sunsetWarriors", __name__, static_folder="static", template_folder="templates")
 
-@sunsetWariorrs.route("/home")
+@sunsetWarriors.route("/home")
 def home():
     return render_template("SW-Home.html")
 
 
-@sunsetWariorrs.route("/about")
+@sunsetWarriors.route("/about")
 def about():
     return render_template("SW-About.html")
 
 
-@sunsetWariorrs.route("/contact")
+@sunsetWarriors.route("/contact")
 def contact():
     return render_template("SW-Contact.html")
 
 
-@sunsetWariorrs.route("/gallery")
+@sunsetWarriors.route("/gallery")
 def gallery():
     return render_template("SW-Gallery.html")
 
 
-@sunsetWariorrs.route("/training")
+@sunsetWarriors.route("/training")
 def training():
     return render_template("SW-Training.html")
 
